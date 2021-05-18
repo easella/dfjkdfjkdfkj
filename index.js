d8234a8418a5549c1313aa2eb36cef6518ee4f39
easella <74786054+easella@users.noreply.github.com>
2021-05-18 12:02:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var proxy = require('express-http-proxy');
 var app = require('express')();
-
+const PORT = process.env.PORT || 8080
 app.use('/proxy', proxy('https://aquapark.io'));
-app.listen(8080)
+app.listen(PORT)
