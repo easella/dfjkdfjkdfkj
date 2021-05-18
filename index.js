41da1b45919fe849e7f20f704982846b0987a56f
easella <74786054+easella@users.noreply.github.com>
2021-05-18 11:47:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
 var proxy = require('express-http-proxy');
-var http=require("http")
-http.globalAgent.maxSockets = Infinity;
-
-var compression = require('compression')
-var cors=require("cors")
-const NodeCache = require( "node-cache" );
-const myCache = new NodeCache();
-
 var app = require('express')();
 
-var server=http.createServer(app)
-app.use(compression())
-app.use(cors())
-
-
-app.use(proxy('https://aquapark.io'));
-
-/*
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});*/
-server.listen(8080)
+app.use('/proxy', proxy('https://aquapark.io'));
+app.listen(8080)
